4156164cd853d448fa0eaaa77035cdcb394d9131
sundematias <matias.sunde@gmail.com>
2023-08-31 22:45:20 +0200

    f

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ if uploaded_file:
     st.markdown(
         "* Check that unusual courses such as HMS0001 has no credits and no grade"
     )
-    st.markdown("Click the fullscreen button to view the whole dataset")
+    st.markdown("Hover over the data to get a fullscreen button")
     st.dataframe(
         df[["Course code", "Letter grade", "Number grade", "Course credits"]],
         800,
